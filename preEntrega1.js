d78a0be099e7702de6bb145be42e924ca19c2cdb
Jorge Chirino <jorgechirino_7@hotmail.com>
2023-02-16 15:15:42 -0300

    correcion funcion

--- preEntrega1.js
+++ preEntrega1.js
@@ -1,24 +1,15 @@
-// Variables de cantidad y precio unitario
-let cantidadSolicitada = Number(prompt("Ingresa la cantidad de este producto que deseas comprar"));
-let precioUnitario = 100;
-
-// Verificar si la cantidad ingresada es válida
-if (isNaN(cantidadSolicitada) || cantidadSolicitada <= 0) {
-  console.log("Error: ingresa una cantidad válida.");
-} else {
-  // Descuentos por cantidad de productos
+// Función para calcular el descuento
+function calcularDescuento(cantidadSolicitada) {
   let cantidad1 = 5;
   let porcentaje1 = 5;
   let cantidad2 = 10;
-  let porcentaje2 = 10;
+  let porcentaje2 = 7.5;
   let cantidad3 = 15;
-  let porcentaje3 = 15;
+  let porcentaje3 = 10;
 
-  // Variable para el descuento
   let descuento = 0;
 
-  // Verificar si la cantidad ingresada corresponde a algún descuento
-  for (let i = 1; i <=3; i++) {
+  for (let i = 1; i <= 3; i++) {
     let cantidad = eval("cantidad" + i);
     let porcentaje = eval("porcentaje" + i);
     if (cantidadSolicitada >= cantidad) {
@@ -26,15 +17,30 @@ if (isNaN(cantidadSolicitada) || cantidadSolicitada <= 0) {
     }
   }
 
+  return descuento;
+}
+
+// Variables de cantidad y precio unitario
+let cantidadSolicitada = Number(
+  prompt("Ingresa la cantidad de este producto que deseas comprar")
+);
+let precioUnitario = 100;
+
+// Verificar si la cantidad ingresada es válida
+if (isNaN(cantidadSolicitada) || cantidadSolicitada <= 0) {
+  console.log("Error: ingresa una cantidad válida.");
+} else {
+  // Calcular el descuento en base a la cantidad solicitada
+  let descuentoFinal = calcularDescuento(cantidadSolicitada);
+
   // Calculo del precio total con el descuento aplicado
-  let precioTotal = cantidadSolicitada * precioUnitario * (1 - descuento / 100);
+  let precioTotal = cantidadSolicitada * precioUnitario * (1 - descuentoFinal / 100);
   let ahorrado = cantidadSolicitada * precioUnitario - precioTotal;
-  let iva = precioTotal * 0.21
-  let precioIva = precioTotal * 1.21
+  let iva = precioTotal * 0.21;
+  let precioIva = precioTotal * 1.21;
 
   console.log("Precio total con descuento= " + precioTotal + " Pesos");
   console.log("Ahorras: " + ahorrado + " Pesos");
   console.log("Iva= " + iva + " Pesos");
   console.log("Precio total con IVA= " + precioIva + " Pesos");
 }
-
